b8bfc6bdd40ef2b03928877313bc4d555947091d
Vivek Vaish <vaishvivek634@gmail.com>
2024-10-03 16:27:23 +0530

    fixing API URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API: 'http://localhost:3000',
-    // API: 'https://notesnook-sand.vercel.app',
+    // API: 'http://localhost:3000',
+    API: 'https://notesnook-sand.vercel.app',
     APP_EMAIL: process.env.APP_EMAIL,
     APP_EMAIL_PASSWORD: process.env.APP_EMAIL_PASSWORD,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
