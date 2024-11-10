b149344c9c82e2f2569ee612e375fd3e0118e3a3
Vivek Vaish <vaishvivek634@gmail.com>
2024-11-10 11:48:24 +0530

    og image added

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,28 @@ export const metadata = {
       url: 'https://www.github.com/Vivekv634',
     },
   ],
+  openGraph: {
+    title: 'AutoBook',
+    description:
+      'AutoBook is an advanced note-taking application with auto-generated notes.',
+    url: 'https://autobook1.vercel.app',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'AutoBook App',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'AutoBook',
+    description:
+      'AutoBook is an advanced note-taking application with auto-generated notes.',
+    image: '/og-image.png',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -31,6 +53,15 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
     >
       <head>
+        {/* <meta property="og:title" content="Your App Title" /> */}
+        {/* <meta */}
+        {/*   property="og:description" */}
+        {/*   content="Description of your application." */}
+        {/* /> */}
+        {/* <meta property="og:image" content="/og-image.png" /> */}
+        {/* <meta property="og:type" content="website" /> */}
+        {/* <meta property="og:url" content="https://yourappdomain.com" /> */}
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="icon" href="/icons/favicon.ico" />
